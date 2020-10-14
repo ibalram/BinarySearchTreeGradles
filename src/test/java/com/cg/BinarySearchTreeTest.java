558108614c941c595b24bb88f32f976e49496754
ibalram <balramsinghrathore3@gmail.com>
2020-10-15 01:58:24 +0530

    Added: Ability to create binary tree and get size

--- src/test/java/com/cg/BinarySearchTreeTest.java
+++ src/test/java/com/cg/BinarySearchTreeTest.java
@@ -2,16 +2,20 @@ package com.cg;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class BinarySearchTreeTest {
 
 	@Test
-	public void testAddIntoBinarySearchTree() {
+	public void testAddIntoBinarySearchTreeAndGetSize() {
 		MyBST bst = new MyBST<Integer>();
-		bst.add(30);
-		bst.add(56);
-		bst.add(70);
+		int[] list = {56,30,70, 22,40,11,3,16,60,95,65, 63, 67};
+		
+		for(int i = 0; i<list.length; ++i)
+			bst.add(list[i]);
+		assertEquals(13, bst.getSize());
 	}
 	
 }
